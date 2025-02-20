fd8001c6b3bc88d7c2211e28cb455fc384b1efe3
Luis Paiva <contato@luispaiva.com.br>
2025-02-19 23:36:17 -0300

    Adição da funcionalidade de CORs.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors'; 
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const dbPromise = open({
